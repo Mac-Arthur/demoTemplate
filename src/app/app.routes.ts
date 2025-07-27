b7a03e580a94b3c96bffb882c8603dbb6a19ef54
Mac-Arthur <macarthur83@yahoo.com>
2025-07-27 09:58:00 +0000

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '../components/layout/layout.component';
+import { HomeComponent } from '../pages/home/home.component';
+import { NotFoundComponent } from '../pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [{ path: 'home', component: HomeComponent, title: 'Home' }],
+  },
+  { path: '**', component: NotFoundComponent, title: 'NotFound' },
+];
